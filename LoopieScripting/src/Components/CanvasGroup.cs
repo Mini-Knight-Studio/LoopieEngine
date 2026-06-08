7b84b6d244ef405bb77a2036c09c7a486699594f
XeivUPC <adria.garcia.almaden@estudiantat.upc.edu>
2026-06-08 12:52:55 +0200

    Fix CanvasGroup Scripting

--- LoopieScripting/src/Components/CanvasGroup.cs
+++ LoopieScripting/src/Components/CanvasGroup.cs
@@ -5,7 +5,7 @@ namespace Loopie
 {
     public sealed class CanvasGroup : Component
     {
-        public float SortingLayer
+        public float Alpha
         {
             get => InternalCalls.CanvasGroup_GetAlpha(entity.ID, ID);
             set => InternalCalls.CanvasGroup_SetAlpha(entity.ID, ID, value);
